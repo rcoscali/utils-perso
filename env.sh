fd93fdfe171fa3d84bfe6a651a2041f247bb703b
Rémi COHEN-SCALI <rcoscali@users.noreply.github.com>
2024-06-11 19:32:32 +0200

    Update env.sh
    Using env for displaying vars

--- env.sh
+++ env.sh
@@ -10,10 +10,14 @@
 # If the var is empty, only add the dir comp. If the env var
 # only contains one comp, add a colon to separate the new
 # dir comp from the other already availables comps.
+# At last the env var is exported.
 #
-# args:
-#  1: directory component to add to a path list env var
-#  2: env var name to which to add the dir comp
+# Args:
+#  $1: directory component to add to a path list env var
+#  $2: env var name to which to add the dir comp
+#
+# Returns:
+#  Nothing
 #
 function add_dir_component_to_var {
     dir_comp=$1
@@ -23,38 +27,49 @@ function add_dir_component_to_var {
     if ! $(eval $grep_cmd); then
 	eval $env_var=$new_val
     fi
+    eval export $env_var
 }
 
 #
-# dell_dir_component_to_var
+# del_dir_component_from_var
 #
 # Function for removing a dir comp to a path list env var as
 # PATH, LD_LIBRARY_PATH, or MANPATH.
+# If the env var is not empty, it process the env var.
 # This function takes care of removing leading colon if some
 # remains.
+# At last, if empty, the environment variable is unset.
 #
-# args:
-#  1: directory component to add to a path list env var
-#  2: env var name to which to add the dir comp
+# Args:
+#  $1: directory component to add to a path list env var
+#  $2: env var name to which to add the dir comp
 #
-function del_dir_component_to_var {
+# Returns:
+#  Nothing
+#
+function del_dir_component_from_var {
     dir_comp=$1
     env_var=$2
-    new_val="$env_var=\$(echo \$${env_var} | sed -e \"s|$dir_comp||g\")"
-    eval $(echo $new_val)
-    new_val="$env_var=\$(echo \$${env_var} | sed -e \"s|::|:|g\")"
-    eval $(echo $new_val)
-    new_val="$env_var=\$(echo \$${env_var} | sed -e \"s|^:||g\")"
-    eval $(echo $new_val)
+    if $(test -n "$env_var"); then
+	new_val="$env_var=\$(echo \$${env_var} | sed -e \"s|$dir_comp||g\")"
+	eval $(echo $new_val)
+	new_val="$env_var=\$(echo \$${env_var} | sed -e \"s|::|:|g\")"
+	eval $(echo $new_val)
+	new_val="$env_var=\$(echo \$${env_var} | sed -e \"s|^:||g\")"
+	eval $(echo $new_val)
+	if $(test -z $env_var); then
+	    unset $(echo $env_var)
+	fi
+    fi
 }
 
 ## If no args or 'add', add dir comps
 if test -z "$1" -o "x$1" == "xadd"; then
-    action=add
+    action=add_dir_component_to_var
 else
     # If arg 'del', delete dir comp
     if test "x$1" == "xdel"; then
-	action=del
+	action=del_dir_component_from_var
     fi
 fi
 
@@ -63,57 +78,54 @@ fi
 ##
 
 # Add local bin to path
-eval ${action}_dir_component_to_var /usr/local/sbin PATH
+eval ${action} /usr/local/sbin PATH
 # Add local sbin to path
-eval ${action}_dir_component_to_var /usr/local/bin PATH
+eval ${action} /usr/local/bin PATH
 
 ##
 ## LD_LIBRARY_PATH
 ##
 
 # Add local lib to ld library path
-eval ${action}_dir_component_to_var /usr/local/lib LD_LIBRARY_PATH
+eval ${action} /usr/local/lib LD_LIBRARY_PATH
 
 ##
 ## LD_RUN_PATH
 ##
 
 # Add local lib to ld run path
-eval ${action}_dir_component_to_var /usr/local/lib LD_RUN_PATH
+eval ${action} /usr/local/lib LD_RUN_PATH
 
 ##
 ## PKG_CONFIG_PATH
 ##
 
-# Add lib pkgconfig dir to pkg config path
-eval ${action}_dir_component_to_var /usr/lib/pkgconfig PKG_CONFIG_PATH
-# Add local lib pkgconfig dir to pkg config path
-eval ${action}_dir_component_to_var /usr/local/lib/pkgconfig PKG_CONFIG_PATH
-# Add local x86_64 lib pkgconfig dir to pkg config path
-eval ${action}_dir_component_to_var /usr/lib/x86_64-linux-gnu/pkgconfig PKG_CONFIG_PATH
+# Add usr lib pkgconfig dir to pkg config path
+eval ${action} /usr/lib/pkgconfig PKG_CONFIG_PATH
+# Add usr lib x86_64 pkgconfig dir to pkg config path
+eval ${action} /usr/lib/x86_64-linux-gnu/pkgconfig PKG_CONFIG_PATH
+# Add usr local lib pkgconfig dir to pkg config path
+eval ${action} /usr/local/lib/pkgconfig PKG_CONFIG_PATH
 
 ##
 ## MANPATH
 ##
 
-# Add local man fr_FR.UTF-8 to man path
-eval ${action}_dir_component_to_var /usr/local/share/man/fr_FR.UTF-8 MANPATH
-# Add local man fr.UTF-8 to man path
-eval ${action}_dir_component_to_var /usr/local/share/man/fr.UTF-8 MANPATH
-# Add local man fr_FR.ISO8859-1 to man path
-eval ${action}_dir_component_to_var /usr/local/share/man/fr_FR.ISO8859-1 MANPATH
-# Add local man fr.ISO8859-1 to man path
-eval ${action}_dir_component_to_var /usr/local/share/man/fr.ISO8859-1 MANPATH
-# Add local man to man path
-eval ${action}_dir_component_to_var /usr/local/share/man MANPATH
+# Add usr local share man fr_FR.UTF-8 to man path
+eval ${action} /usr/local/share/man/fr_FR.UTF-8 MANPATH
+# Add usr local share man fr.UTF-8 to man path
+eval ${action} /usr/local/share/man/fr.UTF-8 MANPATH
+# Add usr local share man fr_FR.ISO8859-1 to man path
+eval ${action} /usr/local/share/man/fr_FR.ISO8859-1 MANPATH
+# Add usr local share man fr.ISO8859-1 to man path
+eval ${action} /usr/local/share/man/fr.ISO8859-1 MANPATH
+# Add usr local share man to man path
+eval ${action} /usr/local/share/man MANPATH
 
 echo "==================================================================================="
 echo "PATH=$PATH"
-echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
-echo "LD_RUN_PATH=$LD_RUN_PATH"
-echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
-echo "MANPATH=$MANPATH"
+env | egrep ^LD_LIBRARY_PATH
+env | egrep ^LD_RUN_PATH
+env | egrep ^PKG_CONFIG_PATH
+env | egrep ^MANPATH
 echo "==================================================================================="
-
-# Export needed env vars
-export PKG_CONFIG_PATH LD_LIBRARY_PATH LD_RUN_PATH MANPATH
