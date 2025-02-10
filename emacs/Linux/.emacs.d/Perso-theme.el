5665202c40f13f170cd08d1be52f0180d58ac9e0
Rémi COHEN-SCALI <rcoscali@users.noreply.github.com>
2025-02-10 14:35:37 +0100

    Create Perso-theme.el

---
+++ emacs/Linux/.emacs.d/Perso-theme.el
@@ -0,0 +1,9 @@
+(deftheme Perso
+  "Created 2024-04-03.")
+
+(custom-theme-set-variables
+ 'Perso
+ '(package-selected-packages '(abyss-theme acme-theme adwaita-dark-theme afternoon-theme ahungry-theme aircon-theme airline-themes alect-themes ample-theme anti-zenburn-theme apropospriate-theme autothemer cyberpunk-theme dracula-theme ef-themes fullframe gotham-theme gruber-darker-theme gruvbox-theme inkpot-theme material-theme modus-themes moe-theme monokai-theme nano-theme oblivion-theme seq solarized-theme spacemacs-theme standard-themes subatomic-theme tangotango-theme theme-buffet tramp-theme ujelly-theme zenburn-theme))
+ '(custom-safe-themes '("058ba0ed929f801fc4077617e816797654c7775382943520875642d5507d8696" "2b8dff32b9018d88e24044eb60d8f3829bd6bbeab754e70799b78593af1c3aba" "32300c2d3e3289dfcbdd757b5e5631a0028c50a4f1714a77dc331fc7114e66b6" "716d3161d37259533ee96fc63d4dce5d323e835d89438336f193f6dcf2163ba5" "ee0785c299c1d228ed30cf278aab82cf1fa05a2dc122e425044e758203f097d2" "f1b2de4bc88d1120782b0417fe97f97cc9ac7c5798282087d4d1d9290e3193bb" "1711947b59ea934e396f616b81f8be8ab98e7d57ecab649a97632339db3a3d19" default)))
+
+(provide-theme 'Perso)
